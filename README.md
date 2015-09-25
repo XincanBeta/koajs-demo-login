26271a0f12167753381c893042cefbe61760f138
wenling-lichao <wenling.lichao@gmail.com>
2015-09-25 09:16:49 +0800

    mysql testing done

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 介绍：
-用 koajs 框架实现登录功能；
+用 koajs 框架实现登录，划分为以下多个阶段：
 
-第零阶段任务：
-* 熟悉 git 和 git flow
+准备阶段任务：
+* 熟悉 git
+* 探索 git flow
 
 第一阶段任务：
-* 测试驱动
+* 测试驱动，记住用 mocha 命令运行
 * 数据存入 mysql 中
 * 使用 session、cookie；sessionid
 * 密码混淆
@@ -18,6 +19,8 @@
 
 
 /*
+  预告：
+
   接下来的工程
   1、即时聊天，并存储数据
   2、用 webservice 与 java 通信
--- test/mysql.js
+++ test/mysql.js
@@ -1,1 +1,62 @@
+/*
+  目标：
+  1、通过 mocha 测试 mysql 的基本功能
+  2、熟悉 node 调用 mysql 的方式
+
+  知识点：
+  0、用 mocha 执行，否则会报错 ReferenceError: describe is not defined
+  1、it 中必须包含 assert，哪怕在空判断
+
+*/
+var assert = require('assert');
 var mysql = require('mysql');
+
+function getConnection(){
+  return mysql.createConnection({
+      host: 'localhost',
+      port: 3306,
+      database: 'test',
+      user: 'admin',
+      password: 'admin'
+    });
+}
+
+describe('Mysql ', function() {
+  var connection;
+  // 传入 done 参数，表示 mocha 处理异步代码
+  it('should connects to database', function(done) {
+    // 通过 query 隐式建立连接
+    connection = getConnection();
+    connection.connect();
+    connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+      if (err) throw err;
+      assert(rows[0].solution == 2); 
+      connection.end();
+      done()
+    });
+  })
+
+  it('should assign an id after being saved', function(done) {
+    connection = getConnection();
+    connection.connect();
+    connection.query('INSERT INTO user SET ?', {username: 'chao', password: '123'}, function(err, result) {
+      if (err) throw err; 
+      // console.log(result);
+      /*
+        { fieldCount: 0,
+          affectedRows: 1,
+          insertId: 2,
+          serverStatus: 2,
+          warningCount: 0,
+          message: '',
+          protocol41: true,
+          changedRows: 0 }
+      */
+      assert(result.insertId)
+      connection.end();
+      done();
+    });
+  })
+
+ 
+})
